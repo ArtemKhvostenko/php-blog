a0b79c0d07ba7d694e3ca5872728db7d3b30843c
Artem Khvostenko <artyom.khvostenko@gmail.com>
2024-03-03 19:14:21 +0200

    step-6 Added logic to render posts list

--- public/index.php
+++ public/index.php
@@ -2,13 +2,14 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 use Blog\PostMapper;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__ . '/../templates');
-$view = new \Twig\Environment($loader);
+$view = new Environment($loader);
 
 $conf = include '../config/db.php';
 $dsn = $conf['dsn'];
@@ -20,37 +21,31 @@ try {
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
+    die("Connection failed: " . $e->getMessage());
 }
 
 $postMapper = new PostMapper($connection);
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) use ($view) {
-    $body = $view->render('index.twig');
+$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $posts = $postMapper->getList('ASC');
+    $body = $view->render('index.twig', ['posts' => $posts]);
     $response->getBody()->write($body);
+
     return $response;
 });
 $app->get('/about', function (Request $request, Response $response, $args) use ($view) {
-    $body = $view->render('about.twig', [
-        'name' => 'John'
-    ]);
+    $body = $view->render('about.twig', ['name' => 'John']);
     $response->getBody()->write($body);
+
     return $response;
 });
 $app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
     $post = $postMapper->getByUrlKey((string) $args['url_key']);
-    if (!$post) {
-        $body = $view->render('not-found.twig');
-    } else {
-        $body = $view->render('post.twig', [
-            'post' => $post
-        ]);
-    }
-
+    $body = (!$post) ? $view->render('not-found.twig'): $view->render('post.twig', ['post' => $post]);
     $response->getBody()->write($body);
+
     return $response;
 });
 
